21f2106337cd5cd83c9dfe8158b305efe512fd2a
Ekaterina Ledeneva <ekiuled@gmail.com>
2022-03-06 21:47:11 +0300

    Easy

---
+++ hw_4/easy.py
@@ -0,0 +1,47 @@
+from threading import Thread
+from multiprocessing import Process
+from timeit import timeit
+from astplot.fibonacci import fibonacci
+
+repetitions = 10
+
+
+def synchronous(n: int):
+    for _ in range(repetitions):
+        fibonacci(n)
+
+
+def threading(n: int):
+    threads = []
+    for _ in range(repetitions):
+        threads.append(Thread(target=fibonacci, args=(n,)))
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+def multiprocessing(n: int):
+    processes = []
+    for _ in range(repetitions):
+        processes.append(Process(target=fibonacci, args=(n,)))
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+
+
+def time(stmt: str, timeit_number: int = 50):
+    seconds = timeit(stmt, number=timeit_number, globals=globals()) / timeit_number
+    print(f'{seconds * 1000 : .3f} ms\t{stmt}')
+
+
+if __name__ == '__main__':
+    n = 10_000
+    time('synchronous(n)')
+    time('threading(n)')
+    time('multiprocessing(n)')
