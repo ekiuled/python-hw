54607808372fea23b62379e4f3ebd4044b6cdc50
Ekaterina Ledeneva <ekiuled@gmail.com>
2022-03-09 19:37:28 +0300

    Hard: logging

--- hw_4/hard.py
+++ hw_4/hard.py
@@ -2,26 +2,44 @@ from multiprocessing import Process, Queue, Pipe
 from multiprocessing.connection import Connection
 import codecs
 from time import sleep
+from datetime import datetime
 
 
-def a(input_queue: Queue, conn: Connection):
-    while True:
-        message = input_queue.get()
-        conn.send(message.lower())
+def logger(filename: str, process_name: str):
+    def log(message: str):
+        with open(filename, 'a') as file:
+            print(f'{datetime.now().strftime("%H:%M:%S")} {process_name} {message}', file=file)
+    return log
+
+
+def a(input_queue: Queue, conn: Connection, log):
+    while (message := input_queue.get()) is not None:
+        message = message.lower()
+        log(f'sends "{message}"')
+        conn.send(message)
         sleep(5)
+    conn.send(None)
 
 
-def b(conn: Connection, output_queue: Queue):
-    while True:
-        message = conn.recv()
-        output_queue.put(codecs.encode(message, 'rot_13'))
+def b(conn: Connection, output_queue: Queue, log):
+    while (message := conn.recv()) is not None:
+        message = codecs.encode(message, 'rot_13')
+        log(f'sends "{message}"')
+        output_queue.put(message)
 
 
-def main(input_queue: Queue, output_queue: Queue):
-    while True:
-        message = input()
-        input_queue.put(message)
-        print(output_queue.get())
+def main(input_queue: Queue, output_queue: Queue, log):
+    try:
+        while True:
+            message = input()
+            log(f'received "{message}"')
+            input_queue.put(message)
+            message = output_queue.get()
+            log(f'prints "{message}"')
+            print(message)
+    except EOFError:
+        input_queue.put(None)
+        log('exits...')
 
 
 if __name__ == '__main__':
@@ -29,12 +47,15 @@ if __name__ == '__main__':
     output_queue = Queue()
     conn_a, conn_b = Pipe()
 
-    process_a = Process(target=a, args=(input_queue, conn_a))
-    process_b = Process(target=b, args=(conn_b, output_queue))
+    LOG_FILENAME = 'artifacts/hard/log.txt'
+
+    process_a = Process(target=a, args=(input_queue, conn_a, logger(LOG_FILENAME, 'A')))
+    process_b = Process(target=b, args=(conn_b, output_queue, logger(LOG_FILENAME, 'B')))
 
     process_a.start()
     process_b.start()
-    main(input_queue, output_queue)
+
+    main(input_queue, output_queue, logger(LOG_FILENAME, 'Main'))
 
     process_a.join()
     process_b.join()
