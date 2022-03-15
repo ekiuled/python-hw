1f6576244d2b3f8ee36c415b48d3ff48a5cc3bc9
Ekaterina Ledeneva <ekiuled@gmail.com>
2022-03-15 22:44:59 +0300

    Hash images

--- hw_5/image_downloader.py
+++ hw_5/image_downloader.py
@@ -2,17 +2,26 @@ import asyncio
 import aiohttp
 import aiofiles
 
+import hashlib
 from datetime import datetime
 import sys
 
 
-async def next_image(url: str, session: aiohttp.ClientSession):
-    async with session.get(url) as response:
-        image = await response.read()
-        timestamp = datetime.now().strftime('%H:%M:%S.%f')[:-4]
+image_hashes = set()
 
-        async with aiofiles.open(f'artifacts/image_{timestamp}.jpg', 'wb') as file:
-            await file.write(image)
+
+async def next_image(url: str, session: aiohttp.ClientSession):
+    while True:
+        async with session.get(url) as response:
+            image = await response.read()
+            hash = hashlib.sha256(image).hexdigest()
+
+            if hash not in image_hashes:
+                image_hashes.add(hash)
+                timestamp = datetime.now().strftime('%H:%M:%S.%f')[:-4]
+                async with aiofiles.open(f'artifacts/image_{timestamp}.jpg', 'wb') as file:
+                    await file.write(image)
+                return
 
 
 async def main(url: str, n: int):
